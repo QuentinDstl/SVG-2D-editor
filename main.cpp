21ad33a2aeecfe89c5a2e41ce026a070c4ff0c14
QuentinDstl <qdsautel@gmail.com>
2019-11-23 19:18:01 +0100

    main commit

--- main.cpp
+++ main.cpp
@@ -19,39 +19,45 @@
 void creerScene(Block* room);
 void dessinerScene(const Block &room);
 
-
-
 int main()
 {
     Block room;
     Block* blockIterateur = &room;
 
+    creerScene(room);
+    dessinerScene(room);
+
     std::ofstream file {FICHIER};
-
-
     if (!file)
     {
-        std::cerr << "Can't write/open sauvegarde.rom\n";
+        std::cerr << "Ouverture impossible du fichier : " << FICHIER;
     }
+    else
+    {
+        room.sauvegarde();
+    }
+<<<<<<< HEAD
 
     creerScene(blockIterateur);
     dessinerScene(room);
+=======
+>>>>>>> a193f9c5ce0bf8f271f1ed94a7de013376d1e68f
 
 
     return 0;
 }
 
+<<<<<<< HEAD
 // TODO : voir pour terminer l'iterateur
 void creerScene(Block* iterateur)
+=======
+void creerScene(Block &room)
+>>>>>>> a193f9c5ce0bf8f271f1ed94a7de013376d1e68f
 {
-
     //ajouterBlock(room, LARGEUR_SCENE, HAUTEUR_SCENE, "room", 0, 0, 0, 0);
     iterateur->ajouterFille(LARGEUR_SCENE, 50, "sol", LARGEUR_SCENE/2, HAUTEUR_SCENE, LARGEUR_SCENE/2, 50);
     iterateur = (iterateur->getFille(0));
 }
-
-
-
 
 void dessinerScene(const Block &room)
 {
