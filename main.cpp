6f6158f3fb01ba27fd7a8a067db3d91edbd83823
clementpellen <clementpellen@gmail.com>
2019-11-30 13:53:15 +0100

    dessin robot

--- main.cpp
+++ main.cpp
@@ -24,9 +24,9 @@ void dessinerScene(const Block &room);
 void toutDessiner(Svgfile &svgout, const Block &room);
 void toutDessinerPlan(Svgfile &svgout,const Block &room, unsigned int plan);
 
-/*
+
 ///Main Version CHARGEMENT depuis fichier
-int main()
+/*int main()
 {
     Block room;
     Block* blockIterateur = &room;
@@ -36,8 +36,8 @@ int main()
     room.chargementScene();
 
     return 0;
-}
-*/
+}*/
+
 
 
 ///Main version CREATION puis sauvegarde
@@ -74,16 +74,27 @@ void creerScene(Block* iterateur)
 
     //on rentre dans les filles
     iterateur = iterateur->getFille(0);
-    //iterateur->getFille(0)->ajouterFilleCercle(0, "cercle", 10, {180,110,50}, {-10,0}, {25,300}, false);
 
     iterateur->ajouterFille(0, "arm1", {25,HAUTEUR_SCENE-50}, {99,0,0}, {0,HAUTEUR_SCENE-50}, {0,0}, false);
-    // iterateur->ajouterFilleBordure(1, {25,HAUTEUR_SCENE-50}, "arm2", {25,HAUTEUR_SCENE-50}, {LARGEUR_SCENE,0}, 0, {200,100,100}, {0,10,0});
-    iterateur->ajouterFilleCercleBordure(0, "block", 25, {50,50,50}, {250,250,250}, {0,25}, {LARGEUR_SCENE/2,0}, true);
-    iterateur = iterateur->getFille(1);
-    //iterateur->ajouterFille(0, "block2", {10,10}, {0,0,0}, {10,0}, {0,0}, false);
-    /*iterateur = iterateur->getFille(0);
-    iterateur->ajouterFilleGlissiere(0,"etagere",{100,40},{190,50,50},{0,20},{25,10},{25,60},false);*/
 
+    iterateur->ajouterFilleBordure(0, "robot", {25,50}, {180,180,40}, {0,0,0}, {12,50}, {LARGEUR_SCENE/2,0}, false);
+    iterateur = iterateur->getFille(1);
+    iterateur->ajouterFilleCercleBordure(0, "tete", 20, {180,180,40}, {0,0,0}, {0,20}, {12,0},false);
+    iterateur = iterateur->getFille(0);
+    iterateur->ajouterFilleCercleBordure(0, "oeil", 6, {200,20,20}, {0,0,0}, {0,0}, {-7,0}, true);
+    iterateur->ajouterFilleCercleBordure(0, "oeil", 6, {200,20,20}, {0,0,0}, {0,0}, {7,0}, true);
+
+    iterateur = iterateur->getMere();
+    iterateur->ajouterFilleBordure(0, "arm", {25,10}, {180,180,40}, {0,0,0}, {0,5}, {25,15}, false);
+    iterateur->ajouterFilleBordure(0, "arm", {25,10}, {180,180,40}, {0,0,0}, {25,5}, {0,15}, false);
+    iterateur->ajouterFilleBordure(0, "", {25,15}, {220,220,60}, {0,0,0}, {0,0}, {0,35}, true);
+
+    iterateur = iterateur->getFille(1);
+    iterateur->ajouterFilleCercleBordure(0, "hand", 5, {220,220,60}, {0,0,0}, {-5,0}, {25,5}, false);
+
+    iterateur = iterateur->getMere();
+    iterateur = iterateur->getFille(2);
+    iterateur->ajouterFilleCercleBordure(0, "hand", 5, {220,220,60}, {0,0,0}, {5,0}, {0,5}, false);
 }
 
 void dessinerScene(const Block &room)
@@ -95,8 +106,8 @@ void dessinerScene(const Block &room)
     //toutDessinerPlan(svgout,room,2);
 
     std::cout << "[i] croix rouge/bleu = position de reference / croix noir/blanc = position de base" << std::endl;
-    room.toutDessinerLiaisons(svgout);
-    room.toutDessinerId(svgout);
+    //room.toutDessinerLiaisons(svgout);
+    //room.toutDessinerId(svgout);
 }
 
 void toutDessiner(Svgfile &svgout,const Block &room)
